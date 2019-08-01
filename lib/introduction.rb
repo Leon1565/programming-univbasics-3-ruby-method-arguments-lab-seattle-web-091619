85df08c7e335582ee485f33e22d232c44e36e382
Cha Yang <chachaiyang3@gmail.com>
2019-08-01 05:59:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,6 +6,6 @@ def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional(name, language)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+def introduction_with_language_optional(name, whatever)
+  puts "Hi, my name is #{name} and I am learning to program in #{whatever}."
 end
